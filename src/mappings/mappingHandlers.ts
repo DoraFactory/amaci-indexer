5d25181c7729ae74c844cd556d857c33c4cec8bf
99Kies <1290017556@qq.com>
2024-08-21 12:47:28 +0800

    update startBlock: amaci 36

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -52,7 +52,7 @@ enum RoundActionType {
 // testnet maci code_id
 const MACI_CODE_ID = [32];
 
-const AMACI_CODE_ID = [35];
+const AMACI_CODE_ID = [36];
 const AMACI_OPERATOR_REGISTRY_CONTRACT =
 	'dora1sqgs383ya7tqdsl7wlzn03qkfuz8qqd3et3laeq6tgrtyw4zxx6sqt4k0s';
 // const SUPPORT_CODE_ID = [13]; // testnet
